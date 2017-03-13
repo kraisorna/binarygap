fafec111dcd0ca086628f5b409dba7eb9792203b
kraisorna <kraisornar@gmail.com>
2017-03-13 17:35:57 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,28 +18,7 @@ namespace BinaryGap
 
     class Solution
     {
-        //Iteration
-        //public int solution(int N)
-        //{
-        //    // write your code in C# 6.0 with .NET 4.5 (Mono)
-        //    var result = Convert.ToString(N, 2);
-        //    int longest = 0;
-        //    int length = 0;
-        //    for (int i = 0; i < result.Length; i++)
-        //    {
-        //        if (result[i] == '0')
-        //            length += 1;
-        //        else
-        //        {
-        //            if (length > longest) { longest = length; }
-        //            length = 0;
-        //        }
-        //    }
-        //    //Console.WriteLine(result);
-        //    //Console.WriteLine(longest);
-        //    return longest;
-        //}
-
+        
         int longest, length=0;
         string result;
 
